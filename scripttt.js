2d947b200ce96ad07798c5fb1d3f1a9ba53389f9
Laurensius-19 <laurensiussinaga1904@gmail.com>
2024-10-17 09:24:36 +0700

    Add files via upload

---
+++ scripttt.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML = "Belajar Inner HTML JS";
+document.write("Ini hasil dari Document.write JS");
+window.alert("Ini alert dari JS");
+
+console.log("Ini hasil Console Log");
+console.info("Ini hasil Console Info");
+console.warn("Ini hasil Console Warning");
+console.error("Ini hasil Console Error");
+
+const dataTable = [
+    {"id": 1, "name": "andre"},
+    {"id": 2, "name": "Joko"}
+]
+console.table(dataTable);
+console.info(dataTable);
